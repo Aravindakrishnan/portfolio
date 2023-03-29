37c2a93d7064e2c240c398b65e9a63165659423d
Aravindakrishnan <aravindapks@gmail.com>
2023-03-29 20:02:50 +0530

    Modified Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
                     <span className="header__icon"><span className="heart">❤️</span></span>
                 </div>
                 <div className="header__body">
-                    <h2 className="header__title">Developer</h2>
+                    <h2 className=" header__title">Developer</h2>
                 </div>
                 <div className="header__footer">
                     <p className="header__desc">Transforming lines of code into something tangible that people can interact with is what fuels my passion for development.</p>
