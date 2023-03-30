d827f4d2646de175f755de6bc2f6f9abd32c1935
Aravindakrishnan <aravindapks@gmail.com>
2023-03-30 14:55:48 +0530

    Added Skills Component

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,18 +1,60 @@
 import "./Skills.css";
 export default function Skills() {
 
-    const frontend_skills = []
-
-    const backend_skills = []
-
-    const cloud_skills = []
-
-    const devops_skills = []
-
+    const skills = [
+        {
+            id: 1,
+            name: "html5"
+        },
+        {
+            id: 2,
+            name: "css3-alt"
+        },
+        {
+            id: 3,
+            name: "square-js"
+        },
+        {
+            id: 4,
+            name: "react fa-spin"
+        },
+        {
+            id: 5,
+            name: "python"
+        },
+        {
+            id: 6,
+            name: "figma"
+        },
+        {
+            id: 7,
+            name: "aws"
+        },
+        {
+            id: 8,
+            name: "github"
+        },
+        {
+            id: 9,
+            name: "AZ"
+        }
+    ]
 
     return (
-        <div className="skills">
-            {/* <i className="fa-brands fa-react" style={{ color: "#fefefe" }}></i> */}
+        <div className="container">
+            <div className="skills">
+                {
+                    skills.map(skill => {
+                        return (
+                            <div key={skill.id} className="skill">
+                                <div className="skill__chip">
+                                    <i className={"icon fa-brands fa-" + skill.name}></i>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
